c1a1aded6407784a6a4c58227ad0c12a02d15bf0
Kaptoiiika <nikita.ozhegov1@mail.ru>
2021-12-08 16:38:21 +0400

    3

--- src/Components/Chat/Chat.js
+++ src/Components/Chat/Chat.js
@@ -1,31 +1,63 @@
 import React from "react"
 import "./Chat.css"
-import Message from './Message/Message.js'
+import Message from "./Message/Message.js"
 
-function addMessage(){
+class Chat extends React.Component {
+  constructor(props) {
+    super(props)
+    this.ChatName = props.ChatName ? props.ChatName : "default"
+    this.userId = props.userId ? props.ChatName : -1
 
-}
+    this.state = {
+      text: "",
+    }
+    this.state.messages = [
+      { id: 0, userId: 1, msg: `Hello world` },
+      { id: 1, userId: 1, msg: `id1 Ne lox` },
+      { id: 2, userId: 1, msg: `Sam lox` },
+    ]
+    this.updateInput = this.updateInput.bind(this)
+  }
 
-function Chat() {
-  addMessage()
+  addMessage(msg) {
+    const buffer = this.state.messages
+    buffer.push({
+      id: this.state.messages.length + 1,
+      userId: this.userId,
+      msg: this.state.text,
+    })
+    this.setState({ messages: buffer })
+  }
+  updateInput(event) {
+    this.setState({ text: event.target.value })
+  }
 
-  return (
-    <div className="Chat">
-      <div className="ChatHeader">
-        <div id="chatName">
-          <h1>
-          loremUllamco
-          </h1>
+  render() {
+    return (
+      <div className="Chat">
+        <div className="ChatHeader">
+          <div id="chatName">
+            <h1>{this.ChatName}</h1>
+          </div>
+        </div>
+
+        <div className="chatConent">
+          {this.state.messages.map((obj) => {
+            return <Message props={obj} />
+          })}
+        </div>
+
+        <div className="messageInput">
+          <input
+            id="messageTextInput"
+            type="text"
+            onChange={this.updateInput}
+          />
+          <button id="messageSendButton" onClick={() => this.addMessage()} />
         </div>
       </div>
-      <div className="chatConent">
-        <Message/>
-      </div>
-      <div className="messageInput">
-        <input id="messageTextInput"/>
-      </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default Chat
--- src/Components/NavSecondary/NavigationSecondary.js
+++ src/Components/NavSecondary/NavigationSecondary.js
@@ -1,22 +1,100 @@
 import React from "react"
 import "./NavigationSecondary.css"
 
-function NavigationSecondary() {
-  return (
-    <div className="NavHeader">
-      <div id="serverName">
-        <h1>Ne lox</h1>
-      </div>
-      <div className="NavigationSecondary">
-        <div className="serverContent">
+class NavigationSecondary extends React.Component {
+  constructor(props) {
+    super(props)
+    this.serverChats = [
+      {
+        id: 0,
+        name: "1 group",
+        chats: [
+          {
+            id: 0,
+            name: "Welcom",
+            messages: [
+              { id: 0, userId: 1, msg: `Hello world` },
+              { id: 1, userId: 1, msg: `id1 Ne lox` },
+              { id: 2, userId: 1, msg: `Sam lox` },
+            ],
+          },
+          {
+            id: 1,
+            name: "goodbay",
+            messages: [
+              { id: 0, userId: 1, msg: `Chat2` },
+              { id: 1, userId: 1, msg: `haha` },
+              { id: 2, userId: 1, msg: `haha` },
+            ],
+          },
+        ],
+      },
+      {
+        id: 1,
+        name: "2 group",
+        chats: [
+          {
+            id: 0,
+            name: "2 Welcom",
+            messages: [
+              { id: 0, userId: 1, msg: `2Hello world` },
+              { id: 1, userId: 1, msg: `2id1 Ne lox` },
+              { id: 2, userId: 1, msg: `2Sam lox` },
+            ],
+          },
+          {
+            id: 1,
+            name: "2 goodbay",
+            messages: [
+              { id: 0, userId: 1, msg: `2Chat2` },
+              { id: 1, userId: 1, msg: `2haha` },
+              { id: 2, userId: 1, msg: `2haha` },
+            ],
+          },
+        ],
+      },
+    ]
+  }
 
-          <h4>He lox</h4>
-          <p>aga</p>
-          <p>aga</p>
+  hideChats(e) {
+    e.target.parentNode.classList.toggle("hide")
+  }
+  
+  changeChat(group,chatObj){
+    console.log(group,chatObj)
+  }
+
+  render() {
+    return (
+      <div className="NavHeader">
+        <div id="serverName">
+          <h1>Ne lox</h1>
+        </div>
+        <div className="NavigationSecondary">
+          <div className="serverContent">
+            {this.serverChats.map((groupObj) => {
+              return (
+                <div className={`GroupChat ${groupObj.id}`}>
+                  <h4 onClick={this.hideChats}>{groupObj.name}</h4>
+
+                  {groupObj.chats.map((chatObj) => {
+                    return (
+                      <p
+                        className="chatName"
+                        onClick={this.changeChat.bind(this, groupObj.id, chatObj.id)}
+                      >
+                        {chatObj.name}
+                      </p>
+                    )
+                  })}
+                </div>
+              )
+            })}
+          </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default NavigationSecondary
